57c841adfcc129ab412c1a8722a0862c1a697235
Avihad Menahem <avihad87@gmail.com>
2017-02-23 21:21:41 +0200

    extract classes

--- src/main/java/CacheOutput.java
+++ src/main/java/CacheOutput.java
@@ -1,6 +1,15 @@
+import java.util.List;
+
 /**
  * Created by testuser on 23/02/2017.
  */
 public class CacheOutput {
+    public int cache;
+    public List<Integer> videos;
 
+    public CacheOutput(int cache, List<Integer> videos)
+    {
+        this.cache = cache;
+        this.videos = videos;
+    }
 }
