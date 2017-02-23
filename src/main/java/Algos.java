427e7512d2bd25570acab539864dd1a2c987e16c
Avihad Menahem <avihad87@gmail.com>
2017-02-23 21:50:32 +0200

    refactor

--- src/main/java/Algos.java
+++ src/main/java/Algos.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,15 +11,42 @@ public class Algos
 {
     public static List<CacheOutput> simple(VideoCache videoCache)
     {
+        int cacheCapacity = videoCache.cacheCapacity;
 
-        return null;
+        List<CacheOutput> outputs = new ArrayList<>();
+        for (int i = 0; i < videoCache.cacheNums; i++)
+        {
+            int currentCapacity = cacheCapacity;
+            List<Integer> currentCache = new ArrayList<>();
+            for (Video video : videoCache.videos.values())
+            {
+                if (video.size < currentCapacity){
+                    currentCache.add(video.index);
+                    currentCapacity -= video.size;
+                }
+            }
+            outputs.add(new CacheOutput(i, currentCache));
+
+        }
+
+
+        return outputs;
     }
 
 
     public static void main(String[] args) throws IOException
     {
         VideoCache videoCache = new VideoCache("kittens.in");
-        System.out.println(videoCache);
+        List<CacheOutput> output = simple(videoCache);
+        outputToFile(output);
+    }
+
+    public static void outputToFile(List<CacheOutput> outputs) throws IOException
+    {
+        List<String> lines = new ArrayList<>();
+        lines.add("" + outputs.size());
+        outputs.stream().map(CacheOutput::toString).forEach(lines::add);
+        Files.write(Paths.get("output.out"), lines);
     }
 
 }
