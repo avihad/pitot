1dde57025bf3f0f5976dfe17491221ec70afdf5e
Avihad Menahem <avihad87@gmail.com>
2017-02-23 20:06:14 +0200

    Initiate commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by amenahem on 2/23/17.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Initiate project pitot");
+    }
+}
